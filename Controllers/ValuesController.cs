6bfae7106ea07c7c15203f0570fff1621810f36b
Saharsh Singh <sahsingh@redhat.com>
2019-01-22 17:34:17 -0500

    Updated Endpoints to be more REST like and have a better coverage of methods

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,36 +13,54 @@ namespace sample_dotnet_app.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+
+        private static long idCounter = 0;
+        private static Dictionary<long, string> values = new Dictionary<long, string>();
+
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public ActionResult<Dictionary<long, string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return values;
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public ActionResult<string> Get(long id)
         {
-            return "value";
+            string value = "";
+            if(!values.TryGetValue(id, out value))
+            {
+                return NotFound("ID not found");
+            }
+            return Ok(value);
         }
 
         // POST api/values
         [HttpPost]
         public void Post([FromBody] string value)
         {
+            values.Add(Interlocked.Increment(ref idCounter), value);
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public ActionResult<string> Put(long id, [FromBody] string value)
         {
+            string existing = "";
+            if(!values.TryGetValue(id, out existing))
+            {
+                return NotFound("ID not found");
+            }
+            values[id] = value;
+            return Ok();
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public void Delete(long id)
         {
+            values.Remove(id);
         }
     }
 }
